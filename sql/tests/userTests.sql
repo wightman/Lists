fc95dcd4e0779cf62173cb4bbe3d71fdcc36c7d9
Rick Wightman <rick@wightmanfamily.org>
2017-01-13 18:50:04 -0400

    outline of user table test script

---
+++ sql/tests/userTests.sql
@@ -0,0 +1,23 @@
+/* userTests.sql
+ *
+ * Assuming an existing Users table
+ * - add a user as admin (succeed)
+ * - add a user as non-admin (FAIL)
+ *
+ * - update an admin status as admin (succeed)
+ * - update an admin status as non-admin (FAIL)
+ *
+ * - update password as user (succeed)
+ * - update password as admin (succeed)
+ * - update password as non-admin (FAIL)
+ *
+ * - add an existing user as admin (FAIL)
+ *
+ * - update a non-existing user as admin (FAIL)
+ *
+ * - remove non-existing record as admin (FAIL)
+ * - remove existing user as the user (succeed)
+ * - remove existing user as admin (succeed)
+ * - remove existing user as non-admin (FAIL)
+ *
+ */

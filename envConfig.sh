f5f3b4ae35be4a4bfcd8784b025eea0f383e3439
Rick Wightman <rick@wightmanfamily.org>
2017-04-17 10:47:32 -0300

    We're goin' Python3!

---
+++ envConfig.sh
@@ -0,0 +1,10 @@
+sudo apt-get install build-essential libssl-dev libffi-dev python-dev
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-venv
+pyvenv env3
+source env3/bin/activate
+pip install flask-restful
+pip install --upgrade pip
+pip3 install flask-restful
+pip install jsondate
+pip install PyMySQL

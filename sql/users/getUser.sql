2b8b3772b973364674fb4774a33fb57fccf11acc
Rick Wightman <wightman@unb.ca>
2017-04-29 21:31:48 -0300

    user procedures corrected/tested OK

--- sql/users/getUser.sql
+++ sql/users/getUser.sql
@@ -7,7 +7,7 @@ BEGIN
    SELECT userId, userName, userEmail, userAdmin, userSince FROM users
       WHERE id = userId;
   /* Error 52702: User not found*/
-  IF( FOUND_ROWS() = 0) THEN
+  IF(FOUND_ROWS() = 0) THEN
     SIGNAL SQLSTATE '52702'
       SET MESSAGE_TEXT = 'User not found.';
   END IF;

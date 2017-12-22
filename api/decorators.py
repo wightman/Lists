d39c54f71ffb36d08e68d7cf38dd64a0032372ff
wightman <wightman@unb.ca>
2017-12-22 18:28:27 -0400

    decorators structured correctly

--- api/decorators.py
+++ api/decorators.py
@@ -3,26 +3,37 @@
 #
 # Rick Wightman, Dec 2017.
 #
+from flask import jsonify, make_response
+from flask import Flask, session
+from flask_session import Session
+
 from functools import wraps
 
 def login_required(func):
     @wraps(func)
     def function_wrapper(*args, **kwargs):
         # Not logged in? We're done.
-        if not 'userid' in session:
+        print('Entered login_required')
+        if not 'userId' in session:
             response = {'status': 'login required'}
             responseCode = 401
             return make_response(jsonify(response), responseCode)
-        return func(*args, **kwargs)
+        print('Login detected')
+        func(*args, **kwargs)
+    return function_wrapper
 
 def admin_required(func):
     @wraps(func)
     def function_wrapper(*args, **kwargs):
         # Not admin? We're done.
-        if not 'admin' in session:
+        print('Entered admin_required')
+        if not 'userAdmin' in session:
             response = {'status': 'admin privileges required'}
             responseCode = 403
+            print('Non-admin detected')
             return make_response(jsonify(response), responseCode)
-        return func(*args, **kwargs)
+        print('Admin detected')
+        func(*args, **kwargs)
+    return function_wrapper
 
 # End.
